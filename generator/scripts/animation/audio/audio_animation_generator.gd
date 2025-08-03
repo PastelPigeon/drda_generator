d1220cc7519432647812d381b32b618a7d2ad234
prototype <3489849684@qq.com>
2025-08-03 12:32:48 +0800

    优化对话音效

--- generator/scripts/animation/audio/audio_animation_generator.gd
+++ generator/scripts/animation/audio/audio_animation_generator.gd
@@ -31,15 +31,6 @@ func generate_audio_animation(dialogues: Array, options: Dictionary) -> Animatio
 			if tokens[token_index]["bbcode_tags"].has("disable_sound"):
 				continue
 			
-			# 如果当前帧有音频播放，则跳过
-			var last_audio_key_index = audio_animation.track_find_key(track_index, char_schedule["absolute_start_time"], Animation.FIND_MODE_NEAREST)
-			if last_audio_key_index != -1:
-				var last_audio_key_end_time = audio_animation.track_get_key_time(track_index, last_audio_key_index) + audio_animation.audio_track_get_key_stream(track_index, last_audio_key_index).get_length()
-				
-				# 当前帧的时间处于上个音频帧的播放时间内，跳过(位于上个音频播放时间内但上个音频时长已不满一帧，不跳过)
-				if char_schedule["absolute_start_time"] <= last_audio_key_end_time and last_audio_key_end_time - char_schedule["absolute_start_time"] >= 1 / options["fps"]:
-					continue
-			
 			var sound_stream = null
 			
 			# 获取当前字符的对话音效资源
@@ -48,6 +39,26 @@ func generate_audio_animation(dialogues: Array, options: Dictionary) -> Animatio
 			else:
 				sound_stream = load(AssetFinder.find_asset(AssetFinder.AssetType.CHARACTER_SOUNDS, "default")[0])
 				
-			audio_animation.audio_track_insert_key(track_index, char_schedule["absolute_start_time"], sound_stream)
+			# 获取上一音频关键帧的index
+			var last_audio_key_index = audio_animation.track_find_key(track_index, char_schedule["absolute_start_time"], Animation.FIND_MODE_NEAREST)
+			
+			# 获取上一音频关键帧的stream的结束时间（当没有上一关键帧时，结束时间为0）
+			var last_audio_key_end_time = 0
+			
+			if last_audio_key_index != -1:
+				# 计算方式：上一音频关键帧的时间 + 上一音频关键帧的stream的时长
+				last_audio_key_end_time = audio_animation.track_get_key_time(track_index, last_audio_key_index) + audio_animation.audio_track_get_key_stream(track_index, last_audio_key_index).get_length()
+				
+			# 判断当前字符绝对时间是否与上一音频关键帧的播放时间重叠，如果重叠，跳过该字符，不添加关键帧
+			if char_schedule["absolute_start_time"] <= last_audio_key_end_time:
+				continue
+				
+			# 根据当前字符时间与上个音频stream结束时间差来判断添加行为
+			if char_schedule["absolute_start_time"] - last_audio_key_end_time <= 1 / options["fps"]:
+				# 当时间差不满一帧时，将音频添加到上个音频流末尾位置
+				audio_animation.audio_track_insert_key(track_index, last_audio_key_end_time, sound_stream)
+			else:
+				# 当时间差超过一帧时，将音频流添加到字符绝对开始时间
+				audio_animation.audio_track_insert_key(track_index, char_schedule["absolute_start_time"], sound_stream)
 			
 	return audio_animation
